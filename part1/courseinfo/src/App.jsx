8fedec3195d1c6f9ff3b898df5fe4052fafaef21
elfynity <devgirl@disroot.org>
2026-05-09 13:28:28 +0200

    added Part component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,10 +9,18 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <>
+    <Part part={props.part} exercise={props.exercise} />
+
+    </>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <>
      <p>
         {props.part}: {props.exercise}
     </p>
-
     </>
   )
 }
