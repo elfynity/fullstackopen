4c97f7fb58e23bc20609ae03e266bd8251748017
elfynity <devgirl@disroot.org>
2026-05-11 16:34:42 +0200

    passing the array values into the components without using map etc

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,42 +6,52 @@ const Header = (props) => {
   )
 }
 
+const Content = (props) => {
+  console.log(props)
+  return(
+    <>
+
+    <p>{props.parts[0].name}:  {props.parts[0].exercises}</p>
+    <p>{props.parts[1].name}:  {props.parts[1].exercises}</p>
+    <p>{props.parts[2].name}:  {props.parts[2].exercises}</p>
+
+    </>
+  )
+}
+
 
 
 const Total = (props) => {
   return(
     <>
-     <p>Number of exercises: {props.totalExercises}</p>
+     <p>Total Exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
+      <Content parts={parts} />
+       <Total parts={parts} />      
 
-      <p>{part1.name} - {part1.exercises}</p>
-      <p>{part2.name} - {part2.exercises}</p>
-      <p>{part3.name} - {part3.exercises}</p>
-
-      <Total totalExercises={part1.exercises + part2.exercises + part3.exercises}/>
-
-     
     </div>
   )
 }
